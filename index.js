569415b16e491bf35cc07975c1702c93f3fea671
Tianxiang Chen <txchendev@gmail.com>
2015-02-25 20:57:46 -0800

    add static serve feature

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * light-server
+ *
+ * Serve, watch, exucute commands and live-reload, all in one.
+ *
+ * Copyright (c) 2015 by Tianxiang Chen
+ */
+
+var connect = require('connect')
+  , serveStatic = require('serve-static')
+  , morgan = require('morgan')
+
+function LightServer(serveDir, options) {
+  var self = this
+  self.serveDir = serveDir
+  self.port = parseInt(options.port) || 4000
+}
+
+LightServer.prototype.start = function() {
+  var self = this
+    , app = connect()
+
+  app.use(morgan('dev'))
+     .use(serveStatic(self.serveDir))
+     .listen(self.port, function() {
+       console.log('listening on http://localhost:' + self.port)
+     })
+}
+
+module.exports = LightServer
