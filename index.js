30903347b16bd9bd5bdcf459c0c68c5db26a9218
Tianxiang Chen <txchendev@gmail.com>
2018-10-18 23:09:58 -0700

    do not log favicon.ico access log

--- index.js
+++ index.js
@@ -40,7 +40,15 @@ LightServer.prototype.start = function () {
   }
 
   var app = connect()
-  !this.options.quiet && app.use(morgan('dev'))
+  if (!this.options.quiet) {
+    app.use(function (req, res, next) {
+      if (req.url === '/favicon.ico') {
+        next()
+      } else {
+        morgan('dev')(req, res, next)
+      }
+    })
+  }
 
   if (!_this.options.noReload) {
     _this.lr = LR({
@@ -48,19 +56,34 @@ LightServer.prototype.start = function () {
       http2: _this.options.http2
     })
     app.use(_this.lr.middleFunc)
-    app.use(injector(
-      function (req, res) {
-        return res.getHeader('content-type') && res.getHeader('content-type').indexOf('text/html') !== -1
-      },
-
-      function (data, req, res, callback) {
-        callback(null, data.toString().replace('</body>', '<script src="/__lightserver__/reload-client.js"></script></body>'))
-      })
+    app.use(
+      injector(
+        function (req, res) {
+          return (
+            res.getHeader('content-type') &&
+            res.getHeader('content-type').indexOf('text/html') !== -1
+          )
+        },
+        function (data, req, res, callback) {
+          callback(
+            null,
+            data
+              .toString()
+              .replace(
+                '</body>',
+                '<script src="/__lightserver__/reload-client.js"></script></body>'
+              )
+          )
+        }
+      )
     )
   }
 
   if (_this.options.serve) {
-    app.use(_this.options.servePrefix || '', serveStatic(_this.options.serve, { extensions: ['html'] }))
+    app.use(
+      _this.options.servePrefix || '',
+      serveStatic(_this.options.serve, { extensions: ['html'] })
+    )
   }
 
   if (_this.options.proxy) {
@@ -78,52 +101,66 @@ LightServer.prototype.start = function () {
   if (_this.options.http2) {
     var fs = require('fs')
     var path = require('path')
-    server = require('spdy').createServer({
-      key: fs.readFileSync(path.join(__dirname, '/localhost.key')),
-      cert: fs.readFileSync(path.join(__dirname, '/localhost.crt'))
-    }, app)
+    server = require('spdy').createServer(
+      {
+        key: fs.readFileSync(path.join(__dirname, '/localhost.key')),
+        cert: fs.readFileSync(path.join(__dirname, '/localhost.crt'))
+      },
+      app
+    )
   } else {
     server = require('http').createServer(app)
   }
 
-  server.listen(_this.options.port, _this.options.bind, function () {
-    var listeningAddr = (_this.options.http2 ? 'https://' : 'http://') +
-      (_this.options.bind || '0.0.0.0') + ':' + _this.options.port
-    console.log('light-server is listening at ' + listeningAddr)
-    if (_this.options.serve) {
-      _this.writeLog('  serving static dir: ' + _this.options.serve)
-      if (_this.options.servePrefix) {
-        _this.writeLog('  using prefix ' + _this.options.servePrefix)
+  server
+    .listen(_this.options.port, _this.options.bind, function () {
+      var listeningAddr =
+        (_this.options.http2 ? 'https://' : 'http://') +
+        (_this.options.bind || '0.0.0.0') +
+        ':' +
+        _this.options.port
+      console.log('light-server is listening at ' + listeningAddr)
+      if (_this.options.serve) {
+        _this.writeLog('  serving static dir: ' + _this.options.serve)
+        if (_this.options.servePrefix) {
+          _this.writeLog('  using prefix ' + _this.options.servePrefix)
+        }
       }
-    }
 
-    if (_this.options.proxy) {
-      _this.writeLog('  when static file not found, proxy to ' + _this.options.proxy)
-      _this.writeLog('    if url path starts with "' + _this.options.proxypaths + '"')
-    }
-
-    _this.writeLog('')
-    if (_this.lr) {
-      _this.lr.startWS(server) // websocket shares same port with http
-    }
-    _this.watch()
-
-    if (_this.options.open) {
-      var opener = require('opener')
-      var openAddr = listeningAddr.replace('://0.0.0.0:', '://localhost:')
-      if (_this.options.servePrefix) {
-        openAddr += _this.options.servePrefix
+      if (_this.options.proxy) {
+        _this.writeLog(
+          '  when static file not found, proxy to ' + _this.options.proxy
+        )
+        _this.writeLog(
+          '    if url path starts with "' + _this.options.proxypaths + '"'
+        )
       }
-      opener(openAddr)
-    }
-  }).on('error', function (err) {
-    if (err.errno === 'EADDRINUSE') {
-      console.log('## ERROR: port ' + _this.options.port + ' is already in use')
-      process.exit(2)
-    } else {
-      console.log(err)
-    }
-  })
+
+      _this.writeLog('')
+      if (_this.lr) {
+        _this.lr.startWS(server) // websocket shares same port with http
+      }
+      _this.watch()
+
+      if (_this.options.open) {
+        var opener = require('opener')
+        var openAddr = listeningAddr.replace('://0.0.0.0:', '://localhost:')
+        if (_this.options.servePrefix) {
+          openAddr += _this.options.servePrefix
+        }
+        opener(openAddr)
+      }
+    })
+    .on('error', function (err) {
+      if (err.errno === 'EADDRINUSE') {
+        console.log(
+          '## ERROR: port ' + _this.options.port + ' is already in use'
+        )
+        process.exit(2)
+      } else {
+        console.log(err)
+      }
+    })
 }
 
 LightServer.prototype.watch = function () {
@@ -140,19 +177,29 @@ LightServer.prototype.watch = function () {
     if (commandToRun || _this.lr) {
       _this.processWatchExp(filesToWatch, commandToRun, reloadOption)
     } else {
-      console.log('## WARNING: Ignoring watch expression "' + we + '", because '
-                  + 'it doesn\'t specify a command and live-reloading is disabled.')
+      console.log(
+        '## WARNING: Ignoring watch expression "' +
+          we +
+          '", because ' +
+          'it doesn\'t specify a command and live-reloading is disabled.'
+      )
     }
   })
 }
 
-LightServer.prototype.processWatchExp = function (filesToWatch, commandToRun, reloadOption) {
+LightServer.prototype.processWatchExp = function (
+  filesToWatch,
+  commandToRun,
+  reloadOption
+) {
   var _this = this
   var gaze = new Gaze(filesToWatch, { interval: _this.options.interval })
 
   // A file has been added/changed/deleted has occurred
-  gaze.on('all', function(event, filepath) {
-    if (gaze.executing) { return }
+  gaze.on('all', function (event, filepath) {
+    if (gaze.executing) {
+      return
+    }
 
     gaze.executing = true
     _this.writeLog('* file: ' + filepath + ' was ' + event)
@@ -167,12 +214,18 @@ LightServer.prototype.processWatchExp = function (filesToWatch, commandToRun, re
 
     console.log('## executing command: ' + commandToRun)
     var start = new Date().getTime()
-    var p = spawn(_this.shell, [_this.firstParam, commandToRun], { stdio: 'inherit' })
+    var p = spawn(_this.shell, [_this.firstParam, commandToRun], {
+      stdio: 'inherit'
+    })
     p.on('close', function (code) {
       if (code !== 0) {
-        console.log('## ERROR: command ' + commandToRun + ' exited with code ' + code)
+        console.log(
+          '## ERROR: command ' + commandToRun + ' exited with code ' + code
+        )
       } else {
-        _this.writeLog('## command succeeded in ' + (new Date().getTime() - start) + 'ms')
+        _this.writeLog(
+          '## command succeeded in ' + (new Date().getTime() - start) + 'ms'
+        )
         if (_this.lr) {
           _this.lr.trigger(reloadOption, _this.options.delay)
         }
@@ -183,13 +236,17 @@ LightServer.prototype.processWatchExp = function (filesToWatch, commandToRun, re
   })
 
   if (filesToWatch.length) {
-    _this.writeLog('light-server is watching these files: ' + filesToWatch.join(', '))
+    _this.writeLog(
+      'light-server is watching these files: ' + filesToWatch.join(', ')
+    )
     _this.writeLog('  when file changes,')
     if (commandToRun) {
       _this.writeLog('  this command will be executed:      ' + commandToRun)
     }
     if (_this.lr) {
-      _this.writeLog('  this event will be sent to browser: ' + reloadOption + '\n')
+      _this.writeLog(
+        '  this event will be sent to browser: ' + reloadOption + '\n'
+      )
     }
   }
 }
